f7d5fdc54a06d0c3a50aa6aa2cdc69b7f31509ad
jpd002 <jpd002@6a6d099a-6a11-0410-877e-d5a07a98cbd2>
2006-10-23 03:21:20 +0000

    git-svn-id: http://svn.purei.org/framework/trunk@80 6a6d099a-6a11-0410-877e-d5a07a98cbd2

--- include/Bitmap.h
+++ include/Bitmap.h
@@ -2,6 +2,7 @@
 #define _BITMAP_H_
 
 #include "Types.h"
+#include <boost/function.hpp>
 
 namespace Framework
 {
@@ -9,15 +10,25 @@ namespace Framework
 	class CBitmap
 	{
 	public:
+		typedef boost::function< void (CBitmap&) > ConstructionFunctionType;
+
 						CBitmap(unsigned int, unsigned int, unsigned int);
+						CBitmap(ConstructionFunctionType);
 						~CBitmap();
+		void			Allocate(unsigned int, unsigned int, unsigned int);
+		bool			IsEmpty() const;
 		unsigned int	GetPixelsSize() const;
+		unsigned int	GetPitch() const;
 		unsigned int	GetWidth() const;
 		unsigned int	GetHeight() const;
 		unsigned int	GetBitsPerPixel() const;
 		uint8*			GetPixels() const;
+		void			Blit(const CBitmap&, unsigned int, unsigned int);
 
 	private:
+						CBitmap(const CBitmap&) {}
+		CBitmap&		operator =(const CBitmap&) {}
+
 		unsigned int	m_nWidth;
 		unsigned int	m_nHeight;
 		unsigned int	m_nBPP;
--- src/Bitmap.cpp
+++ src/Bitmap.cpp
@@ -1,15 +1,25 @@
 #include <malloc.h>
+#include <assert.h>
+#include <exception>
 #include "PtrMacro.h"
 #include "Bitmap.h"
 
 using namespace Framework;
+using namespace std;
 
 CBitmap::CBitmap(unsigned int nWidth, unsigned int nHeight, unsigned int nBPP)
 {
-	m_nWidth	= nWidth;
-	m_nHeight	= nHeight;
-	m_nBPP		= nBPP;
-	m_pPixels	= reinterpret_cast<uint8*>(malloc(GetPixelsSize()));
+	m_pPixels	= NULL;
+	m_nWidth	= 0;
+	m_nHeight	= 0;
+	m_nBPP		= 0;
+	Allocate(nWidth, nHeight, nBPP);
+}
+
+CBitmap::CBitmap(ConstructionFunctionType ConstructionFunction)
+{
+	m_pPixels = NULL;
+	ConstructionFunction(*this);
 }
 
 CBitmap::~CBitmap()
@@ -17,8 +27,24 @@ CBitmap::~CBitmap()
 	FREEPTR(m_pPixels);
 }
 
+void CBitmap::Allocate(unsigned int nWidth, unsigned int nHeight, unsigned int nBPP)
+{
+	assert(m_pPixels == NULL);
+
+	m_nWidth	= nWidth;
+	m_nHeight	= nHeight;
+	m_nBPP		= nBPP;
+	m_pPixels	= reinterpret_cast<uint8*>(malloc(GetPixelsSize()));
+}
+
+bool CBitmap::IsEmpty() const
+{
+	return m_pPixels == NULL;
+}
+
 uint8* CBitmap::GetPixels() const
 {
+	assert(m_pPixels != NULL);
 	return m_pPixels;
 }
 
@@ -42,4 +68,55 @@ unsigned int CBitmap::GetPixelsSize() const
 	unsigned int nSize;
 	nSize = m_nWidth * m_nHeight * m_nBPP;
 	return ((nSize + 7) / 8);
+}
+
+unsigned int CBitmap::GetPitch() const
+{
+	return (((m_nWidth * m_nBPP) + 7) / 8);
+}
+
+void CBitmap::Blit(const CBitmap& Src, unsigned int nLeft, unsigned int nTop)
+{
+	unsigned int nWidth, nHeight;
+	CBitmap& Dst = (*this);
+
+	nWidth	= Src.GetWidth();
+	nHeight	= Src.GetHeight();
+
+	if(&Dst == &Src)
+	{
+		throw exception();
+	}
+
+	if((nWidth + nLeft) > Dst.GetWidth())
+	{
+		throw exception();
+	}
+
+	if((nHeight + nTop) > Dst.GetHeight())
+	{
+		throw exception();
+	}
+
+	if(Dst.GetBitsPerPixel() != Src.GetBitsPerPixel())
+	{
+		throw exception();
+	}
+
+	size_t nSrcPitch;
+	size_t nDstPitch;
+	uint8* pSrcPtr;
+	uint8* pDstPtr;
+
+	nSrcPitch = Src.GetPitch();
+	nDstPitch = Dst.GetPitch();
+	pSrcPtr = Src.GetPixels();
+	pDstPtr = Dst.GetPixels() + (nDstPitch * nTop) + (nLeft * Dst.GetBitsPerPixel() / 8);
+
+	for(unsigned int j = 0; j < nHeight; j++)
+	{
+		memcpy(pDstPtr, pSrcPtr, nSrcPitch);
+		pDstPtr += nDstPitch;
+		pSrcPtr += nSrcPitch;
+	}
 }
