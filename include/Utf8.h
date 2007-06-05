cfe2aef8f01ad1056f8982dafe4e3eaf583572db
jpd002 <jpd002@6a6d099a-6a11-0410-877e-d5a07a98cbd2>
2007-06-05 18:12:58 +0000

    git-svn-id: http://svn.purei.org/framework/trunk@127 6a6d099a-6a11-0410-877e-d5a07a98cbd2

---
+++ include/Utf8.h
@@ -0,0 +1,54 @@
+#ifndef _UTF8_H_
+#define _UTF8_H_
+
+#include <string>
+
+namespace Framework
+{
+    namespace Utf8
+    {
+        template <typename IteratorType>
+        std::wstring ConvertFrom(const IteratorType itBegin, const IteratorType itEnd)
+        {
+            wstring sTemp;
+
+            for(IteratorType itChar(itBegin); itChar != itEnd; itChar++)
+            {
+                char nChar(*itChar);
+                if((nChar & 0x80) == 0)
+                {
+                    sTemp += nChar;
+                }
+                else
+                {
+                    if((nChar & 0xF0) == 0xE0)
+                    {
+                        char nByte1(*(++itChar));
+                        char nByte2(*(++itChar));
+                        
+                        if((nByte1 & 0xC0) != 0x80) throw exception();
+                        if((nByte2 & 0xC0) != 0x80) throw exception();
+
+                        wchar_t nWChar;
+                        nWChar = ((nChar & 0x0F) << 12) | ((nByte1 & 0x3F) << 6) | (nByte2 & 0x3F);
+
+                        sTemp += nWChar;
+                    }
+                    else
+                    {
+                        throw exception();
+                    }
+                }
+            }
+
+            return sTemp;
+        }
+
+        std::wstring ConvertFrom(const std::string& sString)
+        {
+            return ConvertFrom(sString.begin(), sString.end());
+        }
+    };
+}
+
+#endif
