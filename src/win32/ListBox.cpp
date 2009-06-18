1f9372895ebf3623d869bdfc8491ab8359cd47b6
jpd002 <jpd002@6a6d099a-6a11-0410-877e-d5a07a98cbd2>
2009-06-18 00:35:05 +0000

    Fixed small bug where ListBox wasn't using the good default font.
    git-svn-id: http://svn.purei.org/framework/trunk@197 6a6d099a-6a11-0410-877e-d5a07a98cbd2

--- src/win32/ListBox.cpp
+++ src/win32/ListBox.cpp
@@ -1,4 +1,5 @@
 #include "win32/ListBox.h"
+#include "win32/DefaultFonts.h"
 
 using namespace Framework;
 using namespace Framework::Win32;
@@ -6,7 +7,7 @@ using namespace Framework::Win32;
 CListBox::CListBox(HWND hParent, RECT* pR, unsigned long nStyle, unsigned long nStyleEx)
 {
 	Create(nStyleEx, _T("LISTBOX"), _T(""), WS_VISIBLE | WS_CHILD | nStyle, pR, hParent, NULL);
-	SetFont((HFONT)GetStockObject(ANSI_VAR_FONT));
+	SetFont(CDefaultFonts::GetMessageFont());
 }
 
 void CListBox::AddString(const TCHAR* sValue)
