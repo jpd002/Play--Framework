3eeb15d111d81ea610afe00293ad06b2346d19c0
jpd002 <jpd002@6a6d099a-6a11-0410-877e-d5a07a98cbd2>
2010-12-27 20:06:12 +0000

    Added iOS version of string_cast_win1252
    git-svn-id: http://svn.purei.org/framework/trunk@227 6a6d099a-6a11-0410-877e-d5a07a98cbd2

--- src/string_cast_win1252.cpp
+++ src/string_cast_win1252.cpp
@@ -3,13 +3,12 @@
 #ifdef WIN32
 #include <windows.h>
 #elif defined(__APPLE__)
+#include <CoreFoundation/CoreFoundation.h>
 #else
 #include <unicode/ucnv.h>
 #endif
 
-using namespace std;
-
-wstring string_cast_win1252(const string& input)
+std::wstring string_cast_win1252(const std::string& input)
 {
 #ifdef WIN32
 	UINT codePage = 1252;
@@ -17,9 +16,20 @@ wstring string_cast_win1252(const string& input)
 	wchar_t* output = reinterpret_cast<wchar_t*>(alloca((reqLength + 1) * sizeof(wchar_t)));
 	MultiByteToWideChar(codePage, 0, input.c_str(), input.length(), output, reqLength);
 	output[reqLength] = 0;
-	return wstring(output);
+	return std::wstring(output);
 #elif defined(__APPLE__)
-	return wstring(L"???");
+	assert(sizeof(wchar_t) == 4);
+	CFStringEncoding srcEncoding = kCFStringEncodingWindowsLatin1;
+	CFStringEncoding dstEncoding = kCFStringEncodingUTF32;
+	CFStringRef stringRef = CFStringCreateWithBytes(NULL, reinterpret_cast<const UInt8*>(input.c_str()), input.length(), srcEncoding, false);
+	CFIndex length = CFStringGetLength(stringRef);
+	CFIndex bufferSize = length * sizeof(wchar_t);
+	wchar_t* output = reinterpret_cast<wchar_t*>(alloca((length + 1) * sizeof(wchar_t)));
+	CFRange getRange = { 0, length };
+	CFStringGetBytes(stringRef, getRange, dstEncoding, '?', false, reinterpret_cast<UInt8*>(output), bufferSize, NULL);
+	output[length] = 0;
+	CFRelease(stringRef);
+	return std::wstring(output);
 #else
 	assert(0);
 #endif
