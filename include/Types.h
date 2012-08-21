d69c669241070e70338e3d1874cfe84de2da20fe
jpd002 <jpd002@6a6d099a-6a11-0410-877e-d5a07a98cbd2>
2012-08-21 05:02:09 +0000

    Added some static asserts to check type sizes and changed "long" to "int" for 32-bits integers.
    git-svn-id: http://svn.purei.org/framework/trunk@272 6a6d099a-6a11-0410-877e-d5a07a98cbd2

--- include/Types.h
+++ include/Types.h
@@ -1,11 +1,3 @@
-/*
-
-	Framework - Types.h
-	File Version 1.0.000
-	Basic Types Definitions
-
-*/
-
 #ifndef _TYPES_H_
 #define _TYPES_H_
 
@@ -15,15 +7,27 @@ typedef unsigned char uint8;
 typedef short int16;
 typedef unsigned short uint16;
 
-typedef long int32;
+typedef int int32;
 
 //Fixes compilation problem under xcode
 #ifndef _UINT32
 #define _UINT32
-typedef unsigned long uint32;
+typedef unsigned int uint32;
 #endif
 
 typedef long long int64;
 typedef unsigned long long uint64;
+
+static_assert(sizeof(int8) == 1, "int8 size must be 1 byte.");
+static_assert(sizeof(uint8) == 1, "uint8 size must be 1 byte.");
+
+static_assert(sizeof(int16) == 2, "int16 size must be 2 bytes.");
+static_assert(sizeof(uint16) == 2, "uint16 size must be 2 bytes.");
+
+static_assert(sizeof(int32) == 4, "int32 size must be 4 bytes.");
+static_assert(sizeof(uint32) == 4, "uint32 size must be 4 bytes.");
+
+static_assert(sizeof(int64) == 8, "int64 size must be 8 bytes.");
+static_assert(sizeof(uint64) == 8, "uint64 size must be 8 bytes.");
 
 #endif
