e9d597b8654ece5280b1a6882a1aad8aed9304cb
jpd002 <jpd002@6a6d099a-6a11-0410-877e-d5a07a98cbd2>
2013-04-28 03:49:49 +0000

    Added std::make_unique and will remove it when C++14 is available.
    git-svn-id: http://svn.purei.org/framework/trunk@310 6a6d099a-6a11-0410-877e-d5a07a98cbd2

---
+++ include/make_unique.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <memory> // brings in TEMPLATE macros.
+
+namespace std 
+{
+
+#define _MAKE_UNIQUE(TEMPLATE_LIST, PADDING_LIST, LIST, COMMA, X1, X2, X3, X4)  \
+\
+template<class T COMMA LIST(_CLASS_TYPE)>    \
+inline std::unique_ptr<T> make_unique(LIST(_TYPE_REFREF_ARG))    \
+{    \
+    return std::unique_ptr<T>(new T(LIST(_FORWARD_ARG)));    \
+}
+
+_VARIADIC_EXPAND_0X(_MAKE_UNIQUE, , , , )
+#undef _MAKE_UNIQUE
+
+}
