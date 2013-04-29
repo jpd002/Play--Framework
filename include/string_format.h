e2823d8b5c42ae1781a1838e647d96206bc27218
jpd002 <jpd002@6a6d099a-6a11-0410-877e-d5a07a98cbd2>
2013-04-29 04:19:18 +0000

    Added string_format.
    git-svn-id: http://svn.purei.org/framework/trunk@311 6a6d099a-6a11-0410-877e-d5a07a98cbd2

---
+++ include/string_format.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <varargs.h>
+
+std::string string_format(const char* format, ...)
+{
+	int size = 256;
+	std::string result;
+	while(1)
+	{
+		result.resize(size);
+		va_list ap;
+		va_start(ap, format);
+		int n = vsnprintf(const_cast<char*>(result.data()), size, format, ap);
+		va_end(ap);
+		if((n > -1) && (n < size))
+		{
+			result.resize(n);
+			return result;
+		}
+		if (n > -1)
+		{
+			size = n + 1;
+		}
+		else
+		{
+			size *= 2;
+		}
+	}
+	return result;
+}
