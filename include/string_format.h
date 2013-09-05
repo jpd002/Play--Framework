fcae426e3423d6c01e8d750bf11f4199340a2eb3
jpd002 <jpd002@6a6d099a-6a11-0410-877e-d5a07a98cbd2>
2013-09-05 05:25:37 +0000

    Added a string_format that takes a va_list param.
    git-svn-id: http://svn.purei.org/framework/trunk@340 6a6d099a-6a11-0410-877e-d5a07a98cbd2

--- include/string_format.h
+++ include/string_format.h
@@ -3,17 +3,14 @@
 #include <string>
 #include <stdarg.h>
 
-static std::string string_format(const char* format, ...)
+static std::string string_format(const char* format, va_list ap)
 {
 	int size = 256;
 	std::string result;
 	while(1)
 	{
 		result.resize(size);
-		va_list ap;
-		va_start(ap, format);
 		int n = vsnprintf(const_cast<char*>(result.data()), size, format, ap);
-		va_end(ap);
 		if((n > -1) && (n < size))
 		{
 			result.resize(n);
@@ -28,5 +25,14 @@ static std::string string_format(const char* format, ...)
 			size *= 2;
 		}
 	}
+	return result;
+}
+
+static std::string string_format(const char* format, ...)
+{
+	va_list ap;
+	va_start(ap, format);
+	auto result = string_format(format, ap);
+	va_end(ap);
 	return result;
 }
