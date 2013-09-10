2694c2209489aa5457f07b8b1674a94c0bd7c857
jpd002 <jpd002@6a6d099a-6a11-0410-877e-d5a07a98cbd2>
2013-09-10 07:09:07 +0000

    Added wchar_t support to string_format.
    git-svn-id: http://svn.purei.org/framework/trunk@346 6a6d099a-6a11-0410-877e-d5a07a98cbd2

--- include/string_format.h
+++ include/string_format.h
@@ -28,7 +28,33 @@ static std::string string_format(const char* format, va_list ap)
 	return result;
 }
 
-static std::string string_format(const char* format, ...)
+static std::wstring string_format(const wchar_t* format, va_list ap)
+{
+	int size = 256;
+	std::wstring result;
+	while(1)
+	{
+		result.resize(size);
+		int n = vswprintf(const_cast<wchar_t*>(result.c_str()), size, format, ap);
+		if((n > -1) && (n < size))
+		{
+			result.resize(n);
+			return result;
+		}
+		if (n > -1)
+		{
+			size = n + 1;
+		}
+		else
+		{
+			size *= 2;
+		}
+	}
+	return result;
+}
+
+template <typename CharType>
+static std::basic_string<CharType> string_format(const CharType* format, ...)
 {
 	va_list ap;
 	va_start(ap, format);
