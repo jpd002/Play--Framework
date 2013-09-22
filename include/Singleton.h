7a16295a4a80e886d7acd657aae8dc02690805e8
jpd002 <jpd002@6a6d099a-6a11-0410-877e-d5a07a98cbd2>
2013-09-22 05:30:56 +0000

    Added thread-safe singleton.
    git-svn-id: http://svn.purei.org/framework/trunk@348 6a6d099a-6a11-0410-877e-d5a07a98cbd2

--- include/Singleton.h
+++ include/Singleton.h
@@ -1,14 +1,25 @@
-#ifndef _SINGLETON_H_
-#define _SINGLETON_H_
+#pragma once
+
+#include <mutex>
+#include <memory>
 
 template <typename Type> class CSingleton
 {
 public:
 	static Type& GetInstance()
 	{
-		static Type Instance;
-		return Instance;
+		std::call_once(m_onceFlag, 
+			[] ()
+			{
+				m_instance.reset(new Type());
+			}
+		);
+		return *m_instance;
 	}
+
+private:
+	static std::unique_ptr<Type>	m_instance;
+	static std::once_flag			m_onceFlag;
 };
 
 template <typename Type, typename DependantType> class CDependantSingleton
@@ -16,9 +27,22 @@ template <typename Type, typename DependantType> class CDependantSingleton
 public:
 	static Type& GetInstance()
 	{
-		static Type Instance(DependantType::GetInstance());
-		return Instance;
+		std::call_once(m_onceFlag, 
+			[] ()
+			{
+				m_instance.reset(new Type(DependantType::GetInstance()));
+			}
+		);
+		return *m_instance;
 	}
+
+private:
+	static std::unique_ptr<Type>	m_instance;
+	static std::once_flag			m_onceFlag;
 };
 
-#endif
+template <typename Type> std::unique_ptr<Type>	CSingleton<Type>::m_instance;
+template <typename Type> std::once_flag			CSingleton<Type>::m_onceFlag;
+
+template <typename Type, typename DependantType> std::unique_ptr<Type>	CDependantSingleton<Type, DependantType>::m_instance;
+template <typename Type, typename DependantType> std::once_flag			CDependantSingleton<Type, DependantType>::m_onceFlag;
