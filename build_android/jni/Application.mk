6a1b9d3ed12eb9a7a2e57f035d0f77326b63c82d
jpd002 <jpd002@6a6d099a-6a11-0410-877e-d5a07a98cbd2>
2014-09-17 08:04:52 +0000

    Updated Android build config.
    git-svn-id: http://svn.purei.org/framework/trunk@431 6a6d099a-6a11-0410-877e-d5a07a98cbd2

--- build_android/jni/Application.mk
+++ build_android/jni/Application.mk
@@ -1,3 +1,4 @@
-APP_ABI 			:= arm64-v8a
-APP_STL 			:= gnustl_static
-APP_CPPFLAGS 		+= -std=c++11
+APP_ABI 				:= armeabi-v7a
+APP_STL 				:= gnustl_static
+APP_CPPFLAGS 			+= -std=c++11
+NDK_TOOLCHAIN_VERSION 	:= clang
