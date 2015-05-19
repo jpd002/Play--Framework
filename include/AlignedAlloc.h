a27b6f1aba1cb85eb20993a8ecc4d704a10105bc
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-05-19 00:37:01 -0400

    Added AlignedAlloc.

---
+++ include/AlignedAlloc.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <malloc.h>
+#include <stdlib.h>
+
+static void* framework_aligned_alloc(size_t allocSize, size_t alignment)
+{
+#if defined(_MSC_VER)
+	return _aligned_malloc(allocSize, alignment);
+#elif defined(__ANDROID__)
+	void* result = nullptr;
+	if(posix_memalign(&result, alignment, allocSize) != 0)
+	{
+		return nullptr;
+	}
+	return result;
+#else
+	return malloc(allocSize);
+#endif
+}
+
+static void framework_aligned_free(void* ptr)
+{
+#if defined(_MSC_VER)
+	_aligned_free(ptr);
+#else
+	free(ptr);
+#endif
+}
