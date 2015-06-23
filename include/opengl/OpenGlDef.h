10a6b5349ebd7ea30160d5c7cc456903a54b175b
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-06-23 01:02:38 -0400

    Added iOS related includes in OpenGlDef.h

--- include/opengl/OpenGlDef.h
+++ include/opengl/OpenGlDef.h
@@ -1,20 +1,38 @@
 #pragma once
 
 #ifdef WIN32
+
 #include <windows.h>
 #include <GL/glew.h>
 #include <GL/wglew.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+
 #elif defined(__ANDROID__)
+
 #include <EGL/egl.h>
 #include <GLES3/gl3.h>
+
 #elif defined(__APPLE__)
-//TODO: Use separate includes for iOS
+
+#include <TargetConditionals.h>
+
+#if TARGET_OS_IPHONE
+
+//iOS and Simulator
+#import <OpenGLES/ES3/gl.h>
+#import <OpenGLES/ES3/glext.h>
+
+#else
+
+//OSX
 #include <OpenGL/gl3.h>
 #include <OpenGL/glu.h>
 #include <OpenGL/OpenGL.h>
+
 #endif
+
+#endif // defined(__APPLE__)
 
 #ifdef _DEBUG
 #define CHECKGLERROR() { auto errorCode = glGetError(); assert(errorCode == GL_NO_ERROR); }
