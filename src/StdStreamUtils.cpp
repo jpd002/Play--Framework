da04f81c59fee8c6c01e9d7308a9b61c4ecd532f
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-09-04 00:42:25 +0100

    Changed the way StdStreamUtils is built to prevent warnings.

---
+++ src/StdStreamUtils.cpp
@@ -0,0 +1,27 @@
+#include "StdStreamUtils.h"
+
+using namespace Framework;
+
+template <>
+CStdStream Framework::CreateInputStdStream(const std::wstring& path)
+{
+	return CStdStream(path.c_str(), L"rb");
+}
+
+template <>
+CStdStream Framework::CreateInputStdStream(const std::string& path)
+{
+	return CStdStream(path.c_str(), "rb");
+}
+
+template <>
+CStdStream Framework::CreateOutputStdStream(const std::wstring& path)
+{
+	return CStdStream(path.c_str(), L"wb");
+}
+
+template <>
+CStdStream Framework::CreateOutputStdStream(const std::string& path)
+{
+	return CStdStream(path.c_str(), "wb");
+}
