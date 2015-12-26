3f080461596f3d7912caf998cbfe41561e663d6a
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-12-25 21:11:01 -0500

    Added replacement for __builtin_ctz on MSVC.

--- include/BitManip.h
+++ include/BitManip.h
@@ -11,7 +11,15 @@ static unsigned long bitmanip_clz(unsigned long value)
 	return (31 - r);
 }
 
+static unsigned long bitmanip_ctz(unsigned long value)
+{
+	unsigned long r = 0;
+	_BitScanForward(&r, value);
+	return r;
+}
+
 #define __builtin_popcount __popcnt
 #define __builtin_clz bitmanip_clz
+#define __builtin_ctz bitmanip_ctz
 
 #endif
