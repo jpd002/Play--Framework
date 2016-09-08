fd9cdc656be9f685ea7dd929fd6da8c8365cb5ca
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-09-08 10:22:06 -0400

    Rename WRT build to UWP.

---
+++ build_uwp/pch.h
@@ -0,0 +1,14 @@
+﻿//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define NOMINMAX
+
+// Windows Header Files:
+#include <windows.h>
