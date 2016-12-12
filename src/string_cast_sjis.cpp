0c02e2bf7df8800866619998f0beb8c2caa71aa7
C.W. Betts <computers57@hotmail.com>
2016-12-11 17:18:15 -0700

    Implement Mac version of string_cast_sjis.

--- src/string_cast_sjis.cpp
+++ src/string_cast_sjis.cpp
@@ -2,6 +2,8 @@
 #include "alloca_def.h"
 #if defined(_WIN32)
 #include <windows.h>
+#elif defined(__APPLE__)
+#include <CoreFoundation/CoreFoundation.h>
 #elif defined(HAS_ICU)
 #include <unicode/ucnv.h>
 #endif
@@ -17,6 +19,23 @@ wstring string_cast_sjis(const string& input)
 	MultiByteToWideChar(codePage, 0, input.c_str(), input.length(), output, reqLength);
 	output[reqLength] = 0;
 	return wstring(output);
+#elif defined(__APPLE__)
+	assert(sizeof(wchar_t) == 4);
+	CFStringEncoding srcEncoding = kCFStringEncodingShiftJIS;
+	CFStringEncoding dstEncoding = kCFStringEncodingUTF32;
+	CFStringRef stringRef = CFStringCreateWithBytes(NULL, reinterpret_cast<const UInt8*>(input.c_str()), input.length(), srcEncoding, false);
+	if(stringRef == NULL)
+	{
+		return std::wstring(L"(cast failed)");
+	}
+	CFIndex length = CFStringGetLength(stringRef);
+	CFIndex bufferSize = length * sizeof(wchar_t);
+	wchar_t* output = reinterpret_cast<wchar_t*>(alloca((length + 1) * sizeof(wchar_t)));
+	CFRange getRange = { 0, length };
+	CFStringGetBytes(stringRef, getRange, dstEncoding, '?', false, reinterpret_cast<UInt8*>(output), bufferSize, NULL);
+	output[length] = 0;
+	CFRelease(stringRef);
+	return std::wstring(output);
 #elif defined(HAS_ICU)
 	int32_t length = static_cast<int32_t>(input.length());
 	UErrorCode nStatus = U_ZERO_ERROR;
