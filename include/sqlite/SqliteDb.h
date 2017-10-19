9200e2afb960f76ab6829ea288c0ba2bab6e98ef
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2017-10-19 11:04:19 -0400

    Add wrappers for sqlite objects.

---
+++ include/sqlite/SqliteDb.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <sqlite3.h>
+#include <stdexcept>
+#include <cassert>
+#include "Types.h"
+
+namespace Framework
+{
+	class CSqliteDb
+	{
+	public:
+		CSqliteDb() = default;
+
+		CSqliteDb(const char* path, uint32 modeFlags)
+		{
+			int result = sqlite3_open_v2(path, &m_handle, modeFlags, nullptr);
+			if(result != SQLITE_OK)
+			{
+				throw std::runtime_error("Failed to open database.");
+			}
+		}
+
+		//Non-copyable
+		CSqliteDb(const CSqliteDb&) = delete;
+		CSqliteDb& operator =(const CSqliteDb&) = delete;
+
+		CSqliteDb(CSqliteDb&& rhs)
+		{
+			MoveFrom(std::move(rhs));
+		}
+
+		virtual ~CSqliteDb()
+		{
+			Reset();
+		}
+
+		void Reset()
+		{
+			if(m_handle)
+			{
+				int result = sqlite3_close_v2(m_handle);
+				assert(result == SQLITE_OK);
+				m_handle = nullptr;
+			}
+		}
+
+		void MoveFrom(CSqliteDb&& rhs)
+		{
+			std::swap(m_handle, rhs.m_handle);
+		}
+
+		CSqliteDb& operator =(CSqliteDb&& rhs)
+		{
+			Reset();
+			MoveFrom(std::move(rhs));
+			return (*this);
+		}
+
+		operator sqlite3*() const
+		{
+			return m_handle;
+		}
+
+	private:
+		sqlite3* m_handle = nullptr;
+	};
+}
---
+++ include/sqlite/SqliteStatement.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <sqlite3.h>
+#include <stdexcept>
+#include "Types.h"
+
+namespace Framework
+{
+	class CSqliteStatement
+	{
+	public:
+		CSqliteStatement() = default;
+
+		CSqliteStatement(sqlite3* db, const char* query)
+		{
+			int result = sqlite3_prepare_v2(db, query, -1, &m_handle, nullptr);
+			if(result != SQLITE_OK)
+			{
+				throw std::runtime_error("Failed to create statement.");
+			}
+		}
+
+		//Non-copyable
+		CSqliteStatement(const CSqliteStatement&) = delete;
+		CSqliteStatement& operator =(const CSqliteStatement&) = delete;
+
+		virtual ~CSqliteStatement()
+		{
+			Reset();
+		}
+
+		void Reset()
+		{
+			if(m_handle)
+			{
+				int result = sqlite3_finalize(m_handle);
+				assert(result == SQLITE_OK);
+				m_handle = nullptr;
+			}
+		}
+
+		void BindText(int param, const char* value, bool isStatic = false)
+		{
+			int result = sqlite3_bind_text(m_handle, param, value, -1, isStatic ? SQLITE_STATIC : SQLITE_TRANSIENT);
+			assert(result == SQLITE_OK);
+		}
+
+		void BindInteger(int param, uint32 value)
+		{
+			int result = sqlite3_bind_int(m_handle, param, value);
+			assert(result == SQLITE_OK);
+		}
+
+		bool Step()
+		{
+			int result = sqlite3_step(m_handle);
+			assert((result == SQLITE_ROW) || (result == SQLITE_DONE));
+			return (result == SQLITE_ROW);
+		}
+
+		void StepWithResult()
+		{
+			bool hasRow = Step();
+			assert(hasRow);
+		}
+
+		void StepNoResult()
+		{
+			bool hasRow = Step();
+			assert(!hasRow);
+		}
+
+		operator sqlite3_stmt*() const
+		{
+			return m_handle;
+		}
+
+	private:
+		sqlite3_stmt* m_handle = nullptr;
+	};
+}
