73f995761673689290f69f290d9530326e38bc74
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2017-12-23 10:23:48 -0500

    Remove old placeholder stuff.

--- include/placeholder_def.h
+++ include/placeholder_def.h
@@ -1,24 +1,8 @@
-#ifndef _PLACEHOLDER_DEF_H_
-#define _PLACEHOLDER_DEF_H_
+#pragma once
 
-#ifdef _MSC_VER
-
-#define PLACEHOLDER_1   std::tr1::placeholders::_1
-#define PLACEHOLDER_2   std::tr1::placeholders::_2
-#define PLACEHOLDER_3   std::tr1::placeholders::_3
-#define PLACEHOLDER_4   std::tr1::placeholders::_4
-#define PLACEHOLDER_5   std::tr1::placeholders::_5
-#define PLACEHOLDER_6   std::tr1::placeholders::_6
-
-#else
-
-#define PLACEHOLDER_1   _1
-#define PLACEHOLDER_2   _2
-#define PLACEHOLDER_3   _3
-#define PLACEHOLDER_4   _4
-#define PLACEHOLDER_5   _5
-#define PLACEHOLDER_6   _6
-
-#endif
-
-#endif
+#define PLACEHOLDER_1   std::placeholders::_1
+#define PLACEHOLDER_2   std::placeholders::_2
+#define PLACEHOLDER_3   std::placeholders::_3
+#define PLACEHOLDER_4   std::placeholders::_4
+#define PLACEHOLDER_5   std::placeholders::_5
+#define PLACEHOLDER_6   std::placeholders::_6
