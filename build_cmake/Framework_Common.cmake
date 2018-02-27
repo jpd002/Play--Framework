337e7218b07632d7ffe7c4d94b5b2ac09d6cc71f
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2018-02-27 08:50:39 -0500

    Cleanup CMake projects.

---
+++ build_cmake/Framework_Common.cmake
@@ -0,0 +1,28 @@
+cmake_minimum_required(VERSION 2.8)
+
+set(CMAKE_MODULE_PATH
+	${CMAKE_CURRENT_SOURCE_DIR}/../../../Dependencies/cmake-modules
+	${CMAKE_MODULE_PATH}
+)
+
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -D_DEBUG")
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -DNDEBUG")
+
+set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+set(FRAMEWORK_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../../include)
+set(PROJECT_INCLUDES)
+set(PROJECT_LIBS)
+
+if(APPLE)
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -x objective-c++")
+endif(APPLE)
+
+if(WIN32)
+	add_definitions(-D_UNICODE)
+	add_definitions(-DUNICODE)
+	add_definitions(-D_CRT_SECURE_NO_WARNINGS)
+	add_definitions(-D_SCL_SECURE_NO_WARNINGS)
+	add_definitions(-D_LIB)
+endif(WIN32)
