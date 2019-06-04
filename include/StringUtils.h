da7dd3ea0a4fe4378a48d82bb7633b9b376bd8a8
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-06-04 19:41:14 +0100

    Add StringUtils::split based on https://www.fluentcpp.com/2017/04/21/how-to-split-a-string-in-c/

--- include/StringUtils.h
+++ include/StringUtils.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace StringUtils
 {
 	void replace_all(std::string&, const std::string, std::string);
 	void erase_all(std::string&, std::string);
+	void split(std::vector<std::string>&, const std::string&,  char = ' ', bool = false);
 }
--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -1,4 +1,6 @@
 #include "StringUtils.h"
+#include <algorithm>
+#include <sstream>
 
 void StringUtils::replace_all(std::string& str, const std::string replace, std::string replaceWith)
 {
@@ -18,4 +20,24 @@ void StringUtils::replace_all(std::string& str, const std::string replace, std::
 void StringUtils::erase_all(std::string& str, std::string remove)
 {
 	StringUtils::replace_all(str, remove, "");
+}
+
+// split function based on https://www.fluentcpp.com/2017/04/21/how-to-split-a-string-in-c/
+void StringUtils::split(std::vector<std::string>& ret, const std::string& s, char delimiter, bool trim)
+{
+	std::vector<std::string> tokens;
+	std::string token;
+	std::istringstream tokenStream(s);
+	while (std::getline(tokenStream, token, delimiter))
+	{
+		tokens.push_back(token);
+	}
+	if(trim)
+	{
+		for(int i = 0; i < tokens.size(); i++)
+		{
+			std::remove_if(tokens[i].begin(), tokens[i].end(), isspace);
+		}
+	}
+	ret.insert(ret.end(), tokens.begin(), tokens.end());
 }
