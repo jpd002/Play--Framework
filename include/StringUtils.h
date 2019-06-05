2e66c8096fdcf956cc35cbd8037ba01f521eb9eb
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-06-05 22:40:54 +0100

    add StringUtils::trim(...)

--- include/StringUtils.h
+++ include/StringUtils.h
@@ -8,4 +8,8 @@ namespace StringUtils
 	void replace_all(std::string&, const std::string, std::string);
 	void erase_all(std::string&, std::string);
 	void split(std::vector<std::string>&, const std::string&,  char = ' ', bool = false);
+
+	void ltrim(std::string&);
+	void rtrim(std::string&);
+	void trim(std::string&);
 }
--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -1,6 +1,8 @@
 #include "StringUtils.h"
 #include <algorithm>
+#include <cctype>
 #include <sstream>
+#include <iostream>
 
 void StringUtils::replace_all(std::string& str, const std::string replace, std::string replaceWith)
 {
@@ -22,6 +24,32 @@ void StringUtils::erase_all(std::string& str, std::string remove)
 	StringUtils::replace_all(str, remove, "");
 }
 
+void StringUtils::ltrim(std::string& ret)
+{
+	auto itr = ret.begin();
+	while(std::isspace((*itr)))
+	{
+		++itr;
+	}
+	ret.erase(ret.begin(), itr);
+}
+
+void StringUtils::rtrim(std::string& ret)
+{
+	auto itr = ret.rbegin();
+	while(std::isspace((*itr)))
+	{
+		++itr;
+	}
+	ret.erase(itr.base(), ret.end());
+}
+
+void StringUtils::trim(std::string& ret)
+{
+	ltrim(ret);
+	rtrim(ret);
+}
+
 // split function based on https://www.fluentcpp.com/2017/04/21/how-to-split-a-string-in-c/
 void StringUtils::split(std::vector<std::string>& ret, const std::string& s, char delimiter, bool trim)
 {
@@ -36,7 +64,7 @@ void StringUtils::split(std::vector<std::string>& ret, const std::string& s, cha
 	{
 		for(int i = 0; i < tokens.size(); i++)
 		{
-			std::remove_if(tokens[i].begin(), tokens[i].end(), isspace);
+			StringUtils::trim(tokens[i]);
 		}
 	}
 	ret.insert(ret.end(), tokens.begin(), tokens.end());
