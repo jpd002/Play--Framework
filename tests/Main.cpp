de12dddad18954826d42e0b06dcd59e450554a38
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-06-29 08:39:05 -0400

    Add string utils test in main.

--- tests/Main.cpp
+++ tests/Main.cpp
@@ -1,9 +1,11 @@
 #include "BmpTest.h"
 #include "StringCastTest.h"
+#include "StringUtilsTest.h"
 
 int main(int argc, char** argv)
 {
 	BmpTest_Execute();
 	StringCastTest_Execute();
+	StringUtilsTest_Execute();
 	return 0;
 }
