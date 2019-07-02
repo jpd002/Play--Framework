2af8e6f663b90743d0c3189e6344496c7407e645
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-07-02 08:49:35 -0400

    Add tests for CSignal.

--- tests/Main.cpp
+++ tests/Main.cpp
@@ -1,4 +1,5 @@
 #include "BmpTest.h"
+#include "SignalTest.h"
 #include "StringCastTest.h"
 #include "StringUtilsTest.h"
 #include "MathStringUtilsTest.h"
@@ -6,6 +7,7 @@
 int main(int argc, char** argv)
 {
 	BmpTest_Execute();
+	SignalTest_Execute();
 	StringCastTest_Execute();
 	StringUtilsTest_Execute();
 	MathStringUtilsTest_Execute();
---
+++ tests/SignalTest.cpp
@@ -0,0 +1,57 @@
+#include "SignalTest.h"
+#include "Signal.hpp"
+#include <cassert>
+
+void SignalTest_Execute()
+{
+	int value = 0;
+	auto setFct = [&] (int input) { value = input; };
+	auto incFct = [&] () { value++; };
+	
+	{
+		value = 0;
+		Framework::CSignal<void (int)> sig;
+		sig.Connect(setFct);
+		sig(2);
+		assert(value == 0);
+	}
+	
+	{
+		value = 0;
+		Framework::CSignal<void (int)> sig;
+		auto conn = sig.Connect(setFct);
+		sig(2);
+		assert(value == 2);
+	}
+	
+	{
+		value = 0;
+		Framework::CSignal<void (int)> sig;
+		auto conn = sig.Connect(setFct);
+		sig(4);
+		sig(5);
+		conn.reset();
+		sig(6);
+		assert(value == 5);
+	}
+	
+	{
+		value = 0;
+		Framework::CSignal<void (int)> sig;
+		auto conn = sig.ConnectOnce(setFct);
+		sig(4);
+		sig(5);
+		sig(6);
+		assert(value == 4);
+	}
+	
+	{
+		value = 0;
+		Framework::CSignal<void ()> sig;
+		auto connOnce = sig.ConnectOnce(incFct);
+		auto conn = sig.Connect(incFct);
+		sig();
+		sig();
+		assert(value == 3);
+	}
+}
