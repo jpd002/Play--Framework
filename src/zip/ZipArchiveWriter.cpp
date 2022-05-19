ffcd7399e77f3205e27af97b9ff12bc753fea006
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2022-05-19 11:11:17 +0100

    use enum to identify compression methods

--- src/zip/ZipArchiveWriter.cpp
+++ src/zip/ZipArchiveWriter.cpp
@@ -44,7 +44,7 @@ void CZipArchiveWriter::Write(CStream& stream)
         fileHeader.versionNeeded = 0x14;
         fileHeader.compressedSize = 0;
         fileHeader.uncompressedSize = 0;
-        fileHeader.compressionMethod = ZWRAP_isUsingZSTDcompression() ? 93 : 8;
+        fileHeader.compressionMethod = ZWRAP_isUsingZSTDcompression() ? COMPRESSION_METHOD::ZSTD : COMPRESSION_METHOD::DEFLATE;
         fileHeader.fileNameLength = static_cast<uint16>(fileName.length());
         fileHeader.crc = 0;
 
