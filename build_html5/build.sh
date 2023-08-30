743392aa6f6c0e0381196f8a55720e47e76202ab
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2023-08-30 11:35:20 -0400

    Remove useless/unused stuff.

--- build_html5/build.sh
+++
@@ -1,6 +0,0 @@
-export EMSCRIPTEN_ROOT=/Users/jpd002/Projects/emscripten
-mkdir -p build
-cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=${EMSCRIPTEN_ROOT}/cmake/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=Release ..
-make
-cd ..
